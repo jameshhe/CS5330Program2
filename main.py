92e54c3da579074d8ea6804d30eac83a732c10a1
James Huang <hhe990219@hotmail.com>
2021-12-04 13:23:16 -0600

    add(lock manager): added lock manager class, still need to complete the constructor and the functions

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List, Tuple
+
+
 class Database:
     def __init__(self, k: int, nonzero: bool):
         # database to store k integers
@@ -11,6 +14,27 @@ class Database:
 
     def print(self) -> None:
         print(self.database)
+
+
+class LockManager:
+    def __init__(self):
+        self.locks = {}
+
+    # transaction id, kth integer of the database, if the request is for a S-lock
+    # return 1 if lock is granted, 0 if not
+    def request(self, tid: int, k: int, is_s_lock: bool) -> int:
+        granted = False
+        return 1 if granted else 0
+
+    # release all locks held by transaction tid
+    # return the number of locks released
+    def releaseAll(self, tid: int) -> int:
+        locks_released = 0
+        return locks_released
+
+    # return all locks held by tid
+    def showLocks(self, tid: int) -> List[Tuple[int, bool]]:
+        pass
 
 
 if __name__ == '__main__':
