b5c8d45ec6f103047e5fa36aac5beaf3dca92928
James Huang <hhe990219@hotmail.com>
2021-12-04 13:15:50 -0600

    add(database): completed database class

---
+++ main.py
@@ -0,0 +1,18 @@
+class Database:
+    def __init__(self, k: int, nonzero: bool):
+        # database to store k integers
+        self.database = [i + 1 for i in range(k)] if nonzero else [0] * k
+
+    def read(self, k: int) -> int:
+        return self.database[k]
+
+    def write(self, k: int, w: int) -> None:
+        self.database[k] = w
+
+    def print(self) -> None:
+        print(self.database)
+
+
+if __name__ == '__main__':
+    db = Database(10, True)
+    db.print()
