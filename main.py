1a2367d886f9a1674c28864e6e86e50b42fab650
jameshhe <jameshuang@smu.edu>
2021-12-06 15:02:27 -0600

    add(file reading): added reading files through command line arguments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+import sys
 
 
 class Database:
@@ -40,6 +41,7 @@ class LockManager:
 class Transaction:
     def __init__(self, k: int):
         self.local = [i for i in range(k)]
+        self.commands = []
 
     # read the source-th number from the db and set it local[dest]
     def read(self, db: Database, source: int, dest: int) -> None:
@@ -65,10 +67,35 @@ class Transaction:
         self.local[s1] += self.local[s2]
 
     def display(self) -> None:
+        print("Local Database:")
         for num in self.local:
             print(num, end=' ')
+        print("\nCommands:")
+        for command in self.commands:
+            print(command, end=' ')
+        print()
+
+    def add_command(self, operator, operand1, operand2):
+        self.commands.append([operator, operand1, operand2])
+
+
+def read_transaction_file(file_name: str, item_count: int) -> Transaction:
+    transaction = Transaction(item_count)
+    with open(file_name) as file:
+        for i, line in enumerate(file):
+            if i != 0:
+                operator, operand1, operand2 = line.strip().split(" ")
+                transaction.add_command(operator, int(operand1), int(operand2))
+    return transaction
 
 
 if __name__ == '__main__':
-    t = Transaction(10)
-    t.display()
+    item_count, transaction_files = int(sys.argv[1]), sys.argv[2:]
+    # create database
+    DB = Database(item_count, True)
+    transactions = []
+    for file_name in transaction_files:
+        transaction = read_transaction_file(file_name, item_count)
+        transactions.append(transaction)
+    for transaction in transactions:
+        transaction.display()
